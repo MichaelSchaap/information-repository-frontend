6f6392c9c1338177189757b9cf7a0e97a8fa4380
MichaelSchaap <michaelschaap123@gmail.com>
2022-02-07 23:52:13 +0100

    refactor(typing): use RootState and AppDispatch

--- src/features/counter/counterSlice.tsx
+++ src/features/counter/counterSlice.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable no-param-reassign */
 import { createSlice } from '@reduxjs/toolkit';
+import { RootState, AppDispatch } from '../../app/store';
 
 export const slice = createSlice({
   name: 'counter',
@@ -29,9 +30,7 @@ export const { increment, decrement, incrementByAmount } = slice.actions;
 // can be dispatched like a regular action: `dispatch(incrementAsync(10))`. This
 // will call the thunk with the `dispatch` function as the first argument. Async
 // code can then be executed and other actions can be dispatched
-export const incrementAsync = (amount: number) => (
-  dispatch: (arg0: { payload: string; type: string; }) => void,
-) => {
+export const incrementAsync = (amount: number) => (dispatch: AppDispatch): void => {
   setTimeout(() => {
     dispatch(incrementByAmount(amount));
   }, 1000);
@@ -40,6 +39,6 @@ export const incrementAsync = (amount: number) => (
 // The function below is called a selector and allows us to select a value from
 // the state. Selectors can also be defined inline where they're used instead of
 // in the slice file. For example: `useSelector((state) => state.counter.value)`
-export const selectCount = (state: { counter: { value: number; }; }): number => state.counter.value;
+export const selectCount = (state: RootState): number => state.counter.value;
 
 export default slice.reducer;
